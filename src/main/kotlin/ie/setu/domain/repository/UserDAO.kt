bb602d54f5d814f6ecd80b22c517cc2e6348adc0
Karthikeyan Gopal <20099187@mail.wit.ie>
2023-11-01 22:12:35 -0500

    Add test with junit

--- src/main/kotlin/ie/setu/domain/repository/UserDAO.kt
+++ src/main/kotlin/ie/setu/domain/repository/UserDAO.kt
@@ -3,7 +3,7 @@ package ie.setu.domain.repository
 import ie.setu.domain.User
 import ie.setu.domain.db.Users
 import ie.setu.utils.mapToUser
-import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class UserDAO {
@@ -18,19 +18,47 @@ class UserDAO {
     }
 
     fun findById(id: Int): User?{
-        return null
+        return transaction {
+            Users.select() {
+                Users.id eq id}
+                .map{mapToUser(it)}
+                .firstOrNull()
+        }
     }
 
     fun save(user: User){
+        transaction {
+            Users.insert {
+                it[name] = user.name
+                it[email] = user.email
+            }
+        }
     }
 
     fun findByEmail(email: String) :User?{
-        return null
+        return transaction {
+            Users.select() {
+                Users.email eq email}
+                .map{mapToUser(it)}
+                .firstOrNull()
+        }
     }
 
-    fun delete(id: Int) {
+    fun delete(id: Int):Int{
+        return transaction{
+            Users.deleteWhere{
+                Users.id eq id
+            }
+        }
     }
 
     fun update(id: Int, user: User){
+        transaction {
+            Users.update ({
+                Users.id eq id}) {
+                it[name] = user.name
+                it[email] = user.email
+            }
+        }
     }
 }
